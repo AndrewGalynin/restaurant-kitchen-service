3c119a2afb97a2cca75881f1fe9b4c581999749e
Andrii Galynin <galynin69@gmail.com>
2026-05-12 21:50:14 +0300

    feat: add index page with statistics and base bootstrap template

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen.models import Dish, Cook, DishType
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_dishes = Dish.objects.count()
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+
+    context = {
+        "num_dishes": num_dishes,
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+    }
+
+    return render(
+        request,
+        "kitchen/index.html",
+        context=context
+    )
